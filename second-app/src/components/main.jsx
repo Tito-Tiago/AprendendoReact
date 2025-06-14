716a78f4742e651b6031092c1f941165b1daa862
Tiago Tito <tiago.tito@alu.ufc.br>
2025-06-13 22:10:32 -0300

    done styling

--- second-app/src/components/main.jsx
+++ second-app/src/components/main.jsx
@@ -1,7 +1,14 @@
 function Main() {
     return (
         <main>
-            <h1>Main goes here</h1>
+            <h2>Fun facts about React</h2>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 200K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </main>
     )
 }
